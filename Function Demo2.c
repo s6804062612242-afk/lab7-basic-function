62e9ac1a9b430381d0c2ea0671d369d9d33fd43f
6804062612242 chanasuk kongbasad <s6804062612242@email.kmutnb.ac.th>
2026-01-26 11:23:24 +0700

    v.2

--- Function Demo2.c
+++ Function Demo2.c
@@ -4,17 +4,20 @@ int multiply(int n1, int n2);
 int n1,n2;
 int main(){
 	scanf("%d %d",&n1,&n2);
-	checkNumber(n1,n2);
+	if(checkNumber(n1,n2)){
+		printf("%d",multiply(n1,n2));
+	}else{
+		printf("reject");
+	}
 	
 }
 
 int checkNumber(int n1, int n2){
 	if((n1 < 60&&n1 > 20)&&(n2 < 60&&n2 > 20)){
-		printf("%d",multiply(n1,n2));
-		return n1,n2;
+		return 1;
 	}
 	else{
-		printf("reject");
+		return 0;;
 	}
 }
 int multiply(int n1, int n2){
