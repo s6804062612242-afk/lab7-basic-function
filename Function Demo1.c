9c862e2b152cc0802d7dd2b2e3e8017b050ca77f
6804062612242 chanasuk kongbasad <s6804062612242@email.kmutnb.ac.th>
2026-02-09 15:58:42 +0700

    vol.2

--- Function Demo1.c
+++ Function Demo1.c
@@ -1,59 +1,37 @@
-#include<stdio.h> //printf() , scanf()
-#include<math.h>  // ex. Math functions
-#include<ctype.h> // ex. Character manipulation functions
-//prototype
+#include <stdio.h>
+#include <math.h>
 double functionM(void);
 int functionN(int n, double x);
 double functionO(double, int, double, int);
 double functionP(int a, int n, int b, int c);
-int main(void) { //wrong type ruturn
-  	int a, b, c, d, e;
-  	double r, s, t, u, v;
-  	//function calling (a-k)
-  	//a1)
-  	 functionM(); //pass compile 
-  	 //a2)
-  	 printf("%f\n",functionM());
-  	 //b)
-  a = functionM(); //return is double but a is type int;
-  //c)
-  b = functionN(5, 6);
-  printf("%d\n",b);
-  //d)
-  r = functionO(1,a,5,b);
-  printf("%f\n",r); //must format float don't int
-  //e)
-  s = functionP(a,b,c,d); //paramiter wrong chang b is n
-  //f)
-  u = functionM();
-  //g)
-  c = d + functionN(r,s);
-  //h)
-  t = s * functionO(r, a, r, a);
-  //i)
-  a = v + functionP(r, s, t, t);
-  //j)
-  functionP(functionN(a, a), s, t, t+r);
-  //k) 
-  v = functionP(functionN(a, a), s, t, t+r);
-}
-//must have body
-double functionM(void){
-	return 2.0;
-}
-int functionN(int n, double x){
-	//printf("%d %f\n",n,x);
-	return n+x; //b is n+x
-}
-double functionO(double a , int b , double c, int d){
- //have variable
- return a+b+c+d;
-	
-}
-double functionP(int a, int n, int b, int c){
-	
-}
 
+int main(void) {
+    int a, b, c, d, e;
+    double r, s, t, u, v;
+
+    functionM(); 
+    a = functionM(); //pass
+    
+    b = functionN(a, b); //pass have warning b is int but funtion passing parameter (int,long)
+
+    r = functionO(r, a, s, b); //pass  have warning b is int but 
+
+    s = functionP(a, b, c, d, e); //error passing 5 argrument but in prototype have 4 argrument
+
+    u = functionM(); //pass
+    
+    c = d + functionN(r, s); //pass have warning r is double but passing parameter is int
+
+    t = s * functionO(r, a, r, a); //pass
+
+    a = v + functionP(r, s, t, t); //pass but have warning wrong type variable
+
+    functionP(functionN(a, a), s, t, t+r); //pass but have warning wrong type variable must double not int
+
+    v = functionP(functionN(a, a), s, t, t+r); //pass  but have warning wrong type variable must double not int
+
+    return 0;
+}
 
 
 
