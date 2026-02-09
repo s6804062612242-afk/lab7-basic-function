6e630d78f98b97e4836b552dd8118a018e17350d
6804062612242 chanasuk kongbasad <s6804062612242@email.kmutnb.ac.th>
2026-02-09 14:50:21 +0700

    :Build in Function demo

--- Function Demo3.c
+++ Function Demo3.c
@@ -1,39 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int random1to100();
 
 int main() {
-    int N, in, i, j;
-    if (scanf("%d", &N) != 1) return 1;
+    int N,i,j,rnum,numok,check=0;
+    int A[100];
+    scanf("%d",&N);
+    if (N>100) 
+        N=100;
+    srand(time(NULL));  // Initialization, should only be called once.
+                        // To seed the pseudo-random number generator 
+                        // so that it produces a different sequence of 
+                        // "random" numbers each time the program runs 
+    for(i=0;i<N;i++){
+    	A[i] = 0 ;
+	}
+    for (i=0;i<N;i++) {
+        numok=1;
+        while (numok==1)  {
+            rnum=random1to100();
+			check = 0;
+            for (j=0;j<i;j++){
 
-    int arry[N];
-    
-    for(i = 0; i < N; i++) arry[i] = 0;
-
-    for(i = 0; i < N; i++) {
-        scanf("%d", &in);
-
-        int Duplicate = 0;
-        
-        for(j = 0; j < i; j++) {
-            if(in == arry[j]) {
-               Duplicate = 1;
-                break;
-            }
+			if (A[j]==rnum){
+               	check = 1;
+               	break;
+			}
+		}if(check == 0){
+			A[i] = rnum;
+			numok = 2;
+		}
+		
+           }
         }
-
-        if(Duplicate) {
-            printf("been number\n");
-            i--; 
-        } else if(in > 0 && in < 100) {
-            arry[i] = in;
-        } else {
-            printf("reject");
-            i--; 
-        }
-    }
-
-    for(i = 0; i < N; i++) {
-        printf("%d ", arry[i]);
-    }
+        //A[i]=rnum;
+    // display all Finish members
+    for (i=0;i<N;i++){
+	
+        printf("%d ",A[i]);
+  }
     return 0;
+}
+
+int random1to100() {
+    int random_num = (rand() % 100) + 1;
+    //printf("%d", random_num);
+    return random_num;
 }
 
